fdb4f9e576bc5e844aa9095762b1b1e3ba57e5e1
farikkm <farikk.v.2@gmail.com>
2025-06-01 17:52:43 +0500

    menu__list for mobile

--- src/main.js
+++ src/main.js
@@ -5,3 +5,15 @@ import './assets/styles/style.css'
     e.preventDefault();
   })
 })
+
+const menu = document.querySelector(".menu");
+const menuIcon = document.querySelector(".menu__icon");
+
+menuIcon.addEventListener("click", (e) => {
+  if (!menu.classList.contains("_active")) {
+    menu.classList.add("_active")
+  } else {
+    menu.classList.remove("_active")
+  }
+  menuIcon.classList.toggle("_active")
+})
