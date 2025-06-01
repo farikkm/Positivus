b1b595cfdae151ada9a50bb0106685afa3b13932
farikkm <farikk.v.2@gmail.com>
2025-06-01 20:14:35 +0500

    hero section

--- src/main.js
+++ src/main.js
@@ -1,19 +1,29 @@
-import './assets/styles/style.css'
+import "./assets/styles/style.css";
 
 [...document.querySelectorAll("a")].forEach((link) => {
   link.addEventListener("click", (e) => {
     e.preventDefault();
-  })
-})
+  });
+});
 
 const menu = document.querySelector(".menu");
 const menuIcon = document.querySelector(".menu__icon");
 
-menuIcon.addEventListener("click", (e) => {
+menuIcon.addEventListener("click", () => {
   if (!menu.classList.contains("_active")) {
-    menu.classList.add("_active")
+    document.body.classList.add("_lock");
+    menu.classList.add("_active");
   } else {
-    menu.classList.remove("_active")
+    document.body.classList.remove("_lock");
+    menu.classList.remove("_active");
   }
-  menuIcon.classList.toggle("_active")
-})
+  menuIcon.classList.toggle("_active");
+});
+
+const sponsors = document.querySelector(".sponsors");
+
+const fullWrapper = sponsors.parentElement;
+const scrollWidth = sponsors.offsetWidth;
+fullWrapper.style.width = scrollWidth * 2 + "px";
+const duration = scrollWidth / 50; 
+sponsors.style.animationDuration = `${duration}s`;
