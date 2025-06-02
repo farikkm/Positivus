9014a9ce662bfded9486937980f7005e74718223
farikkm <farikk.v.2@gmail.com>
2025-06-02 21:28:17 +0500

    progress bar

--- src/main.js
+++ src/main.js
@@ -8,6 +8,23 @@ import "./assets/styles/style.css";
   });
 });
 
+// Progress Bar
+const progressBarLine = document.getElementById("progress-bar-line");
+const scrollHeight = document.documentElement.scrollHeight;
+const clientHeight = document.documentElement.clientHeight;
+
+function activateProgressBar() {
+  const scrollTop = document.documentElement.scrollTop;
+
+  const progressBarLineWidth = Math.trunc((scrollTop / (scrollHeight - clientHeight)) * 100);
+
+  progressBarLine.style.width = `${progressBarLineWidth}%`
+}
+
+window.addEventListener("DOMContentLoaded", activateProgressBar)
+window.addEventListener("scroll", activateProgressBar)
+
+
 // Menu Icon
 
 const menu = document.querySelector(".menu");
@@ -31,32 +48,59 @@ const sponsors = document.querySelector(".sponsors");
 const fullWrapper = sponsors.parentElement;
 const scrollWidth = sponsors.offsetWidth;
 fullWrapper.style.width = scrollWidth * 2 + "px";
-const duration = scrollWidth / 50; 
+const duration = scrollWidth / 50;
 sponsors.style.animationDuration = `${duration}s`;
+
+// Services
+const servicesBlock = document.getElementById("services");
+const servicesCards = [...document.querySelectorAll(".services__card")];
+
+const servicesObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        console.log("Hello");
+        servicesCards.forEach((card, index) => {
+          setTimeout(() => {
+            card.classList.add("_fade-out");
+            card.classList.remove("_hide-by-opacity");
+          }, index * 300);
+        });
+      }
+    });
+  },
+  { threshold: 0.2 }
+);
+
+if (servicesBlock && servicesCards.length > 0) {
+  servicesObserver.observe(servicesBlock);
+}
 
 // Free proposal
 
-const target = document.querySelector('.free-proposal');
-const decors = [...document.querySelectorAll('.free-proposal__img_decor img')].reverse();
+const freeProposalBlock = document.querySelector(".free-proposal");
+const decors = [
+  ...document.querySelectorAll(".free-proposal__img_decor img"),
+].reverse();
 
-const observer = new IntersectionObserver(
+const freeProposalBlockObserver = new IntersectionObserver(
   (entries) => {
     entries.forEach((entry) => {
       if (entry.isIntersecting && entry.intersectionRatio === 1) {
         decors.forEach((decor, index) => {
           setTimeout(() => {
-            decor.classList.add("_animate");
-            decor.classList.remove("_hide");
-          }, index * 500)
-        })
+            decor.classList.add("_fade-out");
+            decor.classList.remove("_hide-by-opacity");
+          }, index * 400);
+        });
       }
     });
   },
   {
-    threshold: 1.0 // 100% видимость блока
+    threshold: 1.0, // 100% видимость блока
   }
 );
 
-if (target && decors.length > 0) {
-  observer.observe(target);
+if (freeProposalBlock && decors.length > 0) {
+  freeProposalBlockObserver.observe(freeProposalBlock);
 }
